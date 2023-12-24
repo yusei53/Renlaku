f4fe4fc9988b8d4a8d2078b6df2cce3d1aaa0721
yusei53 <yusei530922@gmail.com>
2023-12-24 20:13:46 +0900

    loadingコンポーネント作成

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,15 +1,16 @@
 import { Box, CircularProgress } from "@mui/material";
 
 const Loading = () => {
-  <Box
-    display="flex"
-    alignItems="center"
-    justifyContent="center"
-    width="100px"
-    height="100px"
-  >
-    <CircularProgress color="primary" />
-  </Box>;
+  return (
+    <Box
+      height={"50vh"}
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <CircularProgress color="primary" />
+    </Box>
+  );
 };
 
 export default Loading;
