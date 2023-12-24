f63b951e9760765cb36c72eb469381c047eaf897
yusei53 <yusei530922@gmail.com>
2023-12-24 19:31:58 +0900

    leadingコンポーネントを作成

---
+++ app/loading.tsx
@@ -0,0 +1,15 @@
+import { Box, CircularProgress } from "@mui/material";
+
+const Loading = () => {
+  <Box
+    display="flex"
+    alignItems="center"
+    justifyContent="center"
+    width="100px"
+    height="100px"
+  >
+    <CircularProgress color="primary" />
+  </Box>;
+};
+
+export default Loading;
