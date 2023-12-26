224f25d8aaa8764be7789806e79a79fc03248118
yusei53 <yusei530922@gmail.com>
2023-12-26 21:21:18 +0900

    getCurrentUserと三項演算子を使って認証か未認証かを定義

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div>test</div>;
-}
+import { Box, Typography } from "@mui/material";
+import getCurrentUser from "./actions/getCurrentUser";
+
+export const Home = async () => {
+  const currentUser = await getCurrentUser();
+  return (
+    <Box>
+      {currentUser ? (
+        <Typography>認証中</Typography>
+      ) : (
+        <Typography>未認証</Typography>
+      )}
+    </Box>
+  );
+};
+
+export default Home;
