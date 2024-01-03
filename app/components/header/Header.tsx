12d9e3d5587e5d0261e270ee982dfd73d4ece9d4
yusei53 <yusei530922@gmail.com>
2024-01-03 11:55:39 +0900

    headerから新規登録できるようにする

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,10 +1,17 @@
 "use client";
 
-import * as React from "react";
-import { Box, AppBar, Toolbar, Typography, Button } from "@mui/material";
-import Link from "next/link";
+import { Box, AppBar, Toolbar, Typography } from "@mui/material";
+import { User } from "@prisma/client";
+import { useState } from "react";
+import useSignupModal from "@/app/hooks/useSignupModal";
+import MenuButton from "../menu/menuButton";
+import useLoginModal from "@/app/hooks/useLoginModal";
 
 const Header = () => {
+  const signupModal = useSignupModal();
+  const [isOpen, setIsOpen] = useState(false);
+  const loginModal = useLoginModal();
+
   return (
     <Box component={"header"}>
       <AppBar position="static">
@@ -12,8 +19,20 @@ const Header = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Renlaku
           </Typography>
-          <Link href={"/signup"}>ログイン</Link>
-          <Button color="inherit">新規登録</Button>
+          <MenuButton
+            onClick={() => {
+              loginModal.onOpen();
+              setIsOpen(false);
+            }}
+            label="ログイン"
+          />
+          <MenuButton
+            onClick={() => {
+              signupModal.onOpen();
+              setIsOpen(false);
+            }}
+            label="新規登録"
+          />
         </Toolbar>
       </AppBar>
     </Box>
