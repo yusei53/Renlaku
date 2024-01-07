f0208805f1f41684eb4c3636ee1af0a9be6841c5
yusei53 <yusei530922@gmail.com>
2024-01-08 02:11:07 +0900

    ログアウト機能を実装

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,13 +1,17 @@
 "use client";
-
 import { Box, AppBar, Toolbar, Typography } from "@mui/material";
 import { User } from "@prisma/client";
 import { useState } from "react";
 import useSignupModal from "@/app/hooks/useSignupModal";
 import MenuButton from "../menu/menuButton";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import { signOut } from "next-auth/react";
 
-const Header = () => {
+type TProps = {
+  currentUser: User | null;
+};
+
+const Header: React.FC<TProps> = ({ currentUser }) => {
   const signupModal = useSignupModal();
   const [isOpen, setIsOpen] = useState(false);
   const loginModal = useLoginModal();
@@ -19,20 +23,32 @@ const Header = () => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Renlaku
           </Typography>
-          <MenuButton
-            onClick={() => {
-              loginModal.onOpen();
-              setIsOpen(false);
-            }}
-            label="ログイン"
-          />
-          <MenuButton
-            onClick={() => {
-              signupModal.onOpen();
-              setIsOpen(false);
-            }}
-            label="新規登録"
-          />
+          {currentUser ? (
+            <MenuButton
+              onClick={() => {
+                signOut();
+                setIsOpen(false);
+              }}
+              label={"ログアウト"}
+            />
+          ) : (
+            <>
+              <MenuButton
+                onClick={() => {
+                  loginModal.onOpen();
+                  setIsOpen(false);
+                }}
+                label="ログイン"
+              />
+              <MenuButton
+                onClick={() => {
+                  signupModal.onOpen();
+                  setIsOpen(false);
+                }}
+                label="新規登録"
+              />
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
