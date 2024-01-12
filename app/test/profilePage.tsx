99d3e1a06de1703731c288780c40dd7d3179746b
yusei53 <yusei530922@gmail.com>
2024-01-12 13:40:03 +0900

    profilepage.tsx作成

---
+++ app/test/profilePage.tsx
@@ -0,0 +1,43 @@
+import { Box, Typography } from "@mui/material";
+import { useCallback } from "react";
+import Button from "../components/button/button";
+import Link from "next/link";
+
+type TProps = {
+  onSubmit: () => void;
+  title?: string;
+  body: React.ReactElement;
+  primaryLabel: string;
+  disabled?: boolean;
+};
+
+const ProfilePage: React.FC<TProps> = ({
+  onSubmit,
+  title,
+  body,
+  primaryLabel,
+  disabled,
+}) => {
+  const handleSubmit = useCallback(() => {
+    if (disabled) {
+      return;
+    }
+    onSubmit();
+  }, [onSubmit, disabled]);
+
+  return (
+    <Box>
+      <Typography component="h2">{title}</Typography>
+      <Box>{body}</Box>
+      <Box>
+        <Button
+          disabled={disabled}
+          label={primaryLabel}
+          onClick={handleSubmit}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default ProfilePage;
