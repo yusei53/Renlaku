a23a36f813b5c0b7a29ee3aa3e745bd381d81c2b
yusei53 <yusei530922@gmail.com>
2024-01-17 22:18:47 +0900

    LpButtonコンポーネントの作成

---
+++ app/_components/common/menu/LpButton.tsx
@@ -0,0 +1,45 @@
+import { Box, BoxProps } from "@mui/material";
+
+type TProps = {
+  label: string;
+  onClick: () => void;
+  bgColor?: string;
+  color?: string;
+  hover?: boolean;
+  props?: BoxProps;
+};
+
+const LpButton: React.FC<TProps> = ({
+  label,
+  onClick,
+  bgColor = "#ff5e00",
+  color = "white",
+  hover = false,
+  props,
+}) => {
+  return (
+    <Box
+      component={"button"}
+      p={"8px 14px"}
+      border={"none"}
+      bgcolor={bgColor}
+      color={color}
+      borderRadius={1}
+      boxShadow={"none"}
+      fontSize={"0.9rem"}
+      sx={{
+        cursor: "pointer",
+        transition: "0.3s",
+        "&:hover": {
+          backgroundColor: hover ? "#F84A01" : bgColor,
+        },
+      }}
+      onClick={onClick}
+      {...props}
+    >
+      {label}
+    </Box>
+  );
+};
+
+export default LpButton;
