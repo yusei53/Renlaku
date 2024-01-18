33897658fba9733571d8bb2418b06c00372bc25c
yusei53 <yusei530922@gmail.com>
2024-01-18 22:26:03 +0900

    index.tsを設置

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Header from "./_components/common/header/header";
 import AuthContext from "./_components/context/authContext";
 import ToasterContext from "./_components/context/toasterContext";
 import SignupModal from "./_components/common/modals/signupModal";
 import LoginModal from "./_components/common/modals/loginModal";
 import getCurrentUser from "./actions/getCurrentUser";
+import { Header } from "./_components/common/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
