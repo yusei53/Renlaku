29a992d45340f4327af0fc3c0d845157f83c8fc7
yusei53 <yusei530922@gmail.com>
2024-01-19 19:12:26 +0900

    Checkアイコンコンポーネント作成

---
+++ app/_components/common/question/check.tsx
@@ -0,0 +1,22 @@
+import { BoxProps, styled } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+
+export const Check = () => {
+  return (
+    <Circle>
+      <CheckIcon fontSize={"medium"} style={{ color: "white" }} />
+    </Circle>
+  );
+};
+
+const Circle = styled("div")({
+  backgroundColor: "#006ee4",
+  width: "25px",
+  height: "25px",
+  borderRadius: "50%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+export default Check;
