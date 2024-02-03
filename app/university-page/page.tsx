f6081b5fc37c289b76ab5463eefbd24b839a21c4
yusei53 <yusei530922@gmail.com>
2024-02-04 02:31:42 +0900

    ルーティングを変更/リファクタリングをした

---
+++ app/university-page/page.tsx
@@ -0,0 +1,16 @@
+import { Box } from "@mui/material";
+import getCurrentUser from "../actions/getCurrentUser";
+import FirstView from "./FirstView";
+import UniversityMailContainer from "./UniversityMailContainer";
+
+const Page = async () => {
+  const currentUser = await getCurrentUser();
+  return (
+    <Box>
+      <FirstView currentUser={currentUser} />
+      <UniversityMailContainer currentUser={currentUser} />
+    </Box>
+  );
+};
+
+export default Page;
