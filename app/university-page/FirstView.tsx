62556e8328ad248a7d11d407aea2c61f4c1f6bc3
yusei53 <yusei530922@gmail.com>
2024-02-08 01:40:23 +0900

    FirstViewをリファクタリング

--- app/university-page/FirstView.tsx
+++ app/university-page/FirstView.tsx
@@ -1,24 +1,15 @@
 "use client";
-import {
-  Box,
-  Typography,
-  styled,
-  useMediaQuery,
-  useTheme,
-} from "@mui/material";
+import { Box } from "@mui/material";
 import { User } from "@prisma/client";
-import Link from "next/link";
-import useSignupModal from "../hooks/useSignupModal";
+import Greeting from "./Greeting";
+import Message from "./Message";
+import ProfileLinkOrSignup from "./ProfileLinkOrSignup";
 
 type TProps = {
   currentUser: User | null;
 };
 
 const FirstView: React.FC<TProps> = ({ currentUser }) => {
-  const signupModal = useSignupModal();
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
-
   return (
     <Box
       display={"flex"}
@@ -27,87 +18,15 @@ const FirstView: React.FC<TProps> = ({ currentUser }) => {
       bgcolor={"#016fe5"}
       py={6}
     >
-      {currentUser ? (
-        <Typography color={"white"} fontSize={{ xs: 20, md: 24 }}>
-          {currentUser.email}さんこんにちは！
-        </Typography>
-      ) : (
-        <Typography color={"white"} fontSize={27}>
-          Renlakuへようこそ !
-        </Typography>
-      )}
-      {isSmallScreen ? (
-        <Typography
-          color={"white"}
-          fontSize={{ xs: 20, md: 24 }}
-          py={2}
-          textAlign={"center"}
-        >
-          <Highlight>Renlaku</Highlight>
-          を使って
-          <HighlightCategory>大学を休む時</HighlightCategory>
-          の<br />
-          メール文を作成しよう！
-        </Typography>
-      ) : (
-        <Typography color={"white"} fontSize={27} py={2}>
-          <Highlight>Renlaku</Highlight>
-          を使って
-          <HighlightCategory>大学を休む時</HighlightCategory>の
-          メール文を作成しよう！
-        </Typography>
-      )}
-      {currentUser ? (
-        <Link href={"/profile"}>
-          <Typography
-            color={"white"}
-            sx={{
-              textDecoration: "underline",
-              "&:hover": {
-                color: "#FEDD49",
-                cursor: "pointer",
-              },
-            }}
-          >
-            プロフィール編集はこちらから
-          </Typography>
-        </Link>
-      ) : (
-        <Typography
-          color={"white"}
-          sx={{
-            textDecoration: "underline",
-            "&:hover": {
-              color: "#FEDD49",
-              cursor: "pointer",
-            },
-          }}
-          onClick={() => {
-            signupModal.onOpen();
-          }}
-        >
-          新規登録/ログインでより速くメール文を作成
-        </Typography>
-      )}
+      <Greeting currentUser={currentUser} />
+      <Message highlightTitle={"大学を休む時"} />
+      <ProfileLinkOrSignup
+        currentUser={currentUser}
+        profileLinkContent={"プロフィール編集はこちらから"}
+        signupModalContent={"新規登録/ログインでより速くメールを作成しよう"}
+      />
     </Box>
   );
 };
-
-const Highlight = styled("span")(({ theme }) => ({
-  color: "#FEDD49",
-  fontWeight: "bold",
-  fontSize: 30,
-  [theme.breakpoints.down("sm")]: {
-    fontSize: 25,
-  },
-}));
-
-const HighlightCategory = styled("span")(({ theme }) => ({
-  fontWeight: "bold",
-  fontSize: 30,
-  [theme.breakpoints.down("sm")]: {
-    fontSize: 24,
-  },
-}));
 
 export default FirstView;
