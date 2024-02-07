765282525b938fc823959562fc1a967bd1e4b938
yusei53 <yusei530922@gmail.com>
2024-02-08 01:05:33 +0900

    FirstViewのレスポンシブ対応

--- app/university-page/FirstView.tsx
+++ app/university-page/FirstView.tsx
@@ -1,13 +1,24 @@
 "use client";
-import { Box, Typography, styled } from "@mui/material";
+import {
+  Box,
+  Typography,
+  styled,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import { User } from "@prisma/client";
 import Link from "next/link";
+import useSignupModal from "../hooks/useSignupModal";
 
 type TProps = {
   currentUser: User | null;
 };
 
 const FirstView: React.FC<TProps> = ({ currentUser }) => {
+  const signupModal = useSignupModal();
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <Box
       display={"flex"}
@@ -17,7 +28,7 @@ const FirstView: React.FC<TProps> = ({ currentUser }) => {
       py={6}
     >
       {currentUser ? (
-        <Typography color={"white"} fontSize={24}>
+        <Typography color={"white"} fontSize={{ xs: 20, md: 24 }}>
           {currentUser.email}さんこんにちは！
         </Typography>
       ) : (
@@ -25,13 +36,43 @@ const FirstView: React.FC<TProps> = ({ currentUser }) => {
           Renlakuへようこそ !
         </Typography>
       )}
-      <Typography color={"white"} fontSize={24} py={2}>
-        <Highlight>Renlaku</Highlight>
-        を使って
-        <HighlightCategory>大学を休む時</HighlightCategory>
-        のメール文を作成しよう！
-      </Typography>
-      <Link href="/login">
+      {isSmallScreen ? (
+        <Typography
+          color={"white"}
+          fontSize={{ xs: 20, md: 24 }}
+          py={2}
+          textAlign={"center"}
+        >
+          <Highlight>Renlaku</Highlight>
+          を使って
+          <HighlightCategory>大学を休む時</HighlightCategory>
+          の<br />
+          メール文を作成しよう！
+        </Typography>
+      ) : (
+        <Typography color={"white"} fontSize={27} py={2}>
+          <Highlight>Renlaku</Highlight>
+          を使って
+          <HighlightCategory>大学を休む時</HighlightCategory>の
+          メール文を作成しよう！
+        </Typography>
+      )}
+      {currentUser ? (
+        <Link href={"/profile"}>
+          <Typography
+            color={"white"}
+            sx={{
+              textDecoration: "underline",
+              "&:hover": {
+                color: "#FEDD49",
+                cursor: "pointer",
+              },
+            }}
+          >
+            プロフィール編集はこちらから
+          </Typography>
+        </Link>
+      ) : (
         <Typography
           color={"white"}
           sx={{
@@ -41,23 +82,32 @@ const FirstView: React.FC<TProps> = ({ currentUser }) => {
               cursor: "pointer",
             },
           }}
+          onClick={() => {
+            signupModal.onOpen();
+          }}
         >
-          プロフィール編集はこちらから
+          新規登録/ログインでより速くメール文を作成
         </Typography>
-      </Link>
+      )}
     </Box>
   );
 };
 
-const Highlight = styled("span")({
+const Highlight = styled("span")(({ theme }) => ({
   color: "#FEDD49",
   fontWeight: "bold",
   fontSize: 30,
-});
+  [theme.breakpoints.down("sm")]: {
+    fontSize: 25,
+  },
+}));
 
-const HighlightCategory = styled("span")({
+const HighlightCategory = styled("span")(({ theme }) => ({
   fontWeight: "bold",
   fontSize: 30,
-});
+  [theme.breakpoints.down("sm")]: {
+    fontSize: 24,
+  },
+}));
 
 export default FirstView;
