19bd58dbb7e5f651a04973b9f9bf64a8b8e0a25e
yusei53 <yusei530922@gmail.com>
2024-02-08 03:09:53 +0900

    hooksをジェネリック型にして、使いまわせるようにした

--- app/hooks/useUserData.ts
+++ app/hooks/useUserData.ts
@@ -1,45 +1,19 @@
-import { User } from "@prisma/client";
-import { useEffect, useState } from "react";
-import { UserDataType } from "../types";
+import { useState, useEffect } from "react";
 
-const useUserData = (currentUser: User | null) => {
-  const [userData, setUserData] = useState<UserDataType>({
-    name: null,
-    university: null,
-    teacher: null,
-    grade: null,
-    reasonText: null,
-    date: null,
-    time: null,
-    lesson: null,
-    universityNumber: null,
-    universityEmail: null,
-    phoneNumber: null,
-  });
+function useUserData<T>(initialData: T, currentUser: T | null) {
+  const [userData, setUserData] = useState<T>(initialData);
 
   useEffect(() => {
     if (currentUser) {
-      setUserData({
-        name: currentUser.name,
-        university: currentUser.university,
-        teacher: currentUser.teacher,
-        grade: currentUser.grade,
-        reasonText: currentUser.reasonText,
-        date: currentUser.date,
-        time: currentUser.time,
-        lesson: currentUser.lesson,
-        universityNumber: currentUser.universityNumber,
-        universityEmail: currentUser.universityEmail,
-        phoneNumber: currentUser.phoneNumber,
-      });
+      setUserData(currentUser);
     }
   }, [currentUser]);
 
-  const updateUserData = (field: string, value: string) => {
+  const updateUserData = (field: keyof T, value: string) => {
     setUserData((prev) => ({ ...prev, [field]: value }));
   };
 
   return { userData, updateUserData };
-};
+}
 
 export default useUserData;
