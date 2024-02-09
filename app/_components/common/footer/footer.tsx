91f88bea3d449a3eccb41435dcf794cb3a84fe4c
yusei53 <yusei530922@gmail.com>
2024-02-10 00:48:31 +0900

    footerをリファクタリング

--- app/_components/common/footer/footer.tsx
+++ app/_components/common/footer/footer.tsx
@@ -1,49 +1,28 @@
-"use client";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
-
-import { AppBar, Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export const Footer = () => {
   return (
-    <Box position="relative">
-      <AppBar
-        component="footer"
-        sx={{
-          backgroundColor: "#F7F7F7",
-          p: 6,
-          position: "absolute",
-        }}
-      >
-        <Container maxWidth="lg">
-          <Box
-            display={"flex"}
-            flexDirection={"column"}
-            justifyContent={"center"}
-          >
-            <Typography color="text.primary" gutterBottom>
-              Renlaku
-            </Typography>
-            <Link href="/" color="text.secondary" underline="hover" py={1}>
-              Renlakuとは
-            </Link>
-            <Link
-              href="https://twitter.com/yuseidayo53"
-              color="text.secondary"
-              underline="hover"
-            >
-              開発者
-            </Link>
-          </Box>
-          <Box mt={5}>
-            <Typography color="text.secondary" align="center">
-              {"Copyright © Leapcode inc "}
-              {new Date().getFullYear()}.
-            </Typography>
-          </Box>
-        </Container>
-      </AppBar>
+    <Box component="footer" bgcolor={"#F7F7F7"} p={6}>
+      <Box display={"flex"} flexDirection={"column"} justifyContent={"center"}>
+        <Typography color="text.primary" fontSize={20}>
+          Renlaku
+        </Typography>
+        <Link href="/" color="text.secondary" underline="hover" py={1}>
+          Renlakuとは
+        </Link>
+        <Link
+          href="https://twitter.com/yuseidayo53"
+          color="text.secondary"
+          underline="hover"
+        >
+          開発者
+        </Link>
+      </Box>
+      <Typography color="text.secondary" textAlign={"center"} mt={4}>
+        {"Copyright © Leapcode inc "}
+        {new Date().getFullYear()}.
+      </Typography>
     </Box>
   );
 };
