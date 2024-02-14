a169e199340dcc546b22728c4942942bf48522db
yusei53 <yusei530922@gmail.com>
2024-02-15 01:42:47 +0900

    SkipClassUserTypeとCancelJobOfferUserTypeを定義

--- app/_feature/types/types.ts
+++ app/_feature/types/types.ts
@@ -4,16 +4,27 @@ export type ModalType = {
   onClose: () => void;
 };
 
-export type UserDataType = {
+export type SkipClassUserType = {
+  reasonText: string | null;
   name: string | null;
   university: string | null;
   teacher: string | null;
   grade: string | null;
-  reasonText: string | null;
   date: string | null;
   time: string | null;
   lesson: string | null;
   universityNumber: string | null;
   universityEmail: string | null;
+  phoneNumber: string | null;
+};
+
+export type CancelJobOfferUserType = {
+  reasonText: string | null;
+  company: string | null;
+  companyUserName: string | null;
+  university: string | null;
+  grade: string | null;
+  name: string | null;
+  privateEmail: string | null;
   phoneNumber: string | null;
 };
