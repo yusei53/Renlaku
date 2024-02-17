a42e60c3296af493b53f441c6974c0400911f7ad
yusei53 <yusei530922@gmail.com>
2024-02-18 02:18:40 +0900

    useRefを使って、DOMを参照してコピー機能を実装するために、ref属性を追加した。

--- app/_components/skip-class/university-mail-part.tsx
+++ app/_components/skip-class/university-mail-part.tsx
@@ -1,34 +1,35 @@
 import { Box } from "@mui/material";
 import { SkipClassUserType } from "../../_feature/types/types";
 import UniversityMailText from "./university-mail-text";
+import { useRef } from "react";
+import CopyButton from "../common/copy-button/copy-button";
 
 type TProps = {
   userData: SkipClassUserType;
 };
 
-const UniversityMailPart: React.FC<TProps> = ({ userData }) => (
-  <Box
-    display={"flex"}
-    alignItems={"center"}
-    justifyContent={"center"}
-    mx={2}
-    mt={1}
-    bgcolor={"#D7EEFF"}
-  >
-    <UniversityMailText
-      teacher={userData.teacher || "{ 教授の名前 }"}
-      university={userData.university || "{ 大学/学部/学科 }"}
-      name={userData.name || "{ 氏名 }"}
-      grade={userData.grade || "{ 学年 }"}
-      reasonText={userData.reasonText || "{ ここに欠席理由が挿入されます }"}
-      date={userData.date || "{ 日付 }"}
-      time={userData.time || "{ 何限目 }"}
-      lesson={userData.lesson || "{ 講義名 }"}
-      universityNumber={userData.universityNumber || "{ 学籍番号 }"}
-      universityEmail={userData.universityEmail || "{ メールアドレス }"}
-      phoneNumber={userData.phoneNumber || "{ 電話番号 }"}
-    />
-  </Box>
-);
+const UniversityMailPart: React.FC<TProps> = ({ userData }) => {
+  const textRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <Box mx={2} mt={1} bgcolor={"#D7EEFF"} position={"relative"}>
+      <CopyButton textRef={textRef} />
+      <UniversityMailText
+        ref={textRef}
+        teacher={userData.teacher || "{ 教授の名前 }"}
+        university={userData.university || "{ 大学/学部/学科 }"}
+        name={userData.name || "{ 氏名 }"}
+        grade={userData.grade || "{ 学年 }"}
+        reasonText={userData.reasonText || "{ ここに欠席理由が挿入されます }"}
+        date={userData.date || "{ 日付 }"}
+        time={userData.time || "{ 何限目 }"}
+        lesson={userData.lesson || "{ 講義名 }"}
+        universityNumber={userData.universityNumber || "{ 学籍番号 }"}
+        universityEmail={userData.universityEmail || "{ メールアドレス }"}
+        phoneNumber={userData.phoneNumber || "{ 電話番号 }"}
+      />
+    </Box>
+  );
+};
 
 export default UniversityMailPart;
