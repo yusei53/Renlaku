14d8b718bbe081785f219df6e48a78682b6a8fb1
yusei53 <yusei530922@gmail.com>
2024-02-18 02:17:52 +0900

    CopyButtonコンポーネント作成

---
+++ app/_components/common/copy-button/copy-button.tsx
@@ -0,0 +1,33 @@
+import { IconButton, Tooltip } from "@mui/material";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { useState, useCallback, RefObject } from "react";
+
+type TProps = {
+  textRef: RefObject<HTMLDivElement>;
+};
+
+const CopyButton: React.FC<TProps> = ({ textRef }) => {
+  const [openTip, setOpenTip] = useState(false);
+
+  const copyToClipboard = useCallback(() => {
+    const text = textRef.current?.innerText || "";
+    navigator.clipboard.writeText(text);
+    setOpenTip(true);
+    setTimeout(() => setOpenTip(false), 1000);
+  }, [textRef]);
+
+  return (
+    <Tooltip
+      open={openTip}
+      placement="top"
+      title="Copied!"
+      sx={{ position: "absolute", top: 12, right: 15 }}
+    >
+      <IconButton color="primary" size="medium" onClick={copyToClipboard}>
+        <ContentCopyIcon fontSize="medium" />
+      </IconButton>
+    </Tooltip>
+  );
+};
+
+export default CopyButton;
