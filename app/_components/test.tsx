ae1fd93a54911d80e72e6b8254708dba727d397d
yusei53 <yusei530922@gmail.com>
2024-02-20 18:12:16 +0900

    panelをテストで作ってみた

--- app/_components/test.tsx
+++ app/_components/test.tsx
@@ -3,16 +3,14 @@ import { Tabs, Tab, Box } from "@mui/material";
 import { useRouter, useSearchParams } from "next/navigation";
 import { User } from "@prisma/client";
 import { useEffect, useState } from "react";
-import CancelOfferFirstView from "../_components/cancel-job-offer/cancel-offer-firstview";
-import CancelOfferContainer from "../_components/cancel-job-offer/cancel-offer-container";
-import UniversityFirstView from "../_components/skip-class/university-firstview";
-import UniversityMailContainer from "../_components/skip-class/university-mail-container";
+import TopTabPanel from "./common/tab/top-tab-panel";
+import BottomTabPanel from "./common/tab/bottom-tab-panel";
 
 export default function Test({ currentUser }: { currentUser: User | null }) {
-  const [tabValue, setTabValue] = useState("大学の欠席");
+  const [tabValue, setTabValue] = useState("skip-class");
   const router = useRouter();
   const searchParams = useSearchParams();
-  const fffsearchParams = searchParams.get("aaa");
+  const fffsearchParams = searchParams.get("category");
 
   useEffect(() => {
     if (fffsearchParams) {
@@ -23,78 +21,32 @@ export default function Test({ currentUser }: { currentUser: User | null }) {
   }, [fffsearchParams]);
 
   const handleChange = (event: any, newValue: any) => {
-    router.push(window.location.pathname + "/?aaa=" + newValue);
+    router.push(window.location.pathname + "/?category=" + newValue);
 
     // 2次リリースでshallow routing optionを実現したい
     // https://github.com/vercel/next.js/discussions/48320
+    // https://medium.com/@moh.mir36/shallow-routing-with-next-js-v13-app-directory-2d765928c340
   };
 
   return (
     <Box>
-      {tabValue === "大学の欠席" && (
-        <TabHeadPanel value={tabValue} currentUser={currentUser} />
+      {tabValue === "skip-class" && (
+        <TopTabPanel value={tabValue} currentUser={currentUser} />
       )}
-      {tabValue === "tab2" && (
-        <TabHeadPanel value={tabValue} currentUser={currentUser} />
+      {tabValue === "cancel-job-offer" && (
+        <TopTabPanel value={tabValue} currentUser={currentUser} />
       )}
-      <Tabs
-        value={tabValue}
-        onChange={handleChange}
-        aria-label="simple tabs example"
-        variant="fullWidth"
-      >
-        <Tab label="kkkk" value="大学の欠席" />
-        <Tab label="tab2" value="tab2" />
+      <Tabs value={tabValue} onChange={handleChange} centered>
+        <Tab label="大学の欠席" value="skip-class" />
+        <Tab label="内定取り消し" value="cancel-job-offer" />
         <Tab label="tab3" value="tab3" />
       </Tabs>
-      {tabValue === "大学の欠席" && (
-        <TabPanel value={tabValue} currentUser={currentUser} />
+      {tabValue === "skip-class" && (
+        <BottomTabPanel value={tabValue} currentUser={currentUser} />
       )}
-      {tabValue === "tab2" && (
-        <TabPanel value={tabValue} currentUser={currentUser} />
+      {tabValue === "cancel-job-offer" && (
+        <BottomTabPanel value={tabValue} currentUser={currentUser} />
       )}
-    </Box>
-  );
-}
-
-function TabHeadPanel({
-  value,
-  currentUser,
-}: {
-  value: string;
-  currentUser: any;
-}) {
-  return (
-    <Box role="tabpanel">
-      <Box>
-        {value === "大学の欠席" && (
-          <CancelOfferFirstView currentUser={currentUser} />
-        )}
-        {value === "tab2" && <UniversityFirstView currentUser={currentUser} />}
-      </Box>
-    </Box>
-  );
-}
-
-function TabPanel({
-  value,
-
-  currentUser,
-}: {
-  value: string;
-
-  currentUser: any;
-}) {
-  return (
-    <Box role="tabpanel">
-      <Box>
-        {value === "大学の欠席" && (
-          <CancelOfferContainer currentUser={currentUser} />
-        )}
-        {value === "tab2" && (
-          <UniversityMailContainer currentUser={currentUser} />
-        )}
-      </Box>
     </Box>
   );
 }
