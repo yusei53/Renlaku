baa402e00b683d80bd9f4223a4fdbfc07c40eb9a
yusei53 <yusei530922@gmail.com>
2024-02-21 14:11:37 +0900

    UI微調整

--- app/_components/cancel-job-offer/cancel-offer-input-part.tsx
+++ app/_components/cancel-job-offer/cancel-offer-input-part.tsx
@@ -22,7 +22,7 @@ const CancelOfferInputPart: React.FC<TProps> = ({
   updateUserData,
 }) => {
   return (
-    <Box textAlign={"center"} mt={{ sm: 5 }}>
+    <Box textAlign={"center"} mt={{ sm: 1 }}>
       <Box>
         <Typography pb={"4%"}>step1. 何を辞退するか選択してね！</Typography>
         <FormControl sx={{ width: "230px" }}>
@@ -31,7 +31,7 @@ const CancelOfferInputPart: React.FC<TProps> = ({
               mt: "-8px",
             }}
           >
-            欠席理由
+            辞退カテゴリ
           </InputLabel>
           <CustomSelect
             value={userData.reasonText}
