da57b23f0bc07bf0c1c20a3c39e09d21c5814a3a
yusei53 <yusei530922@gmail.com>
2024-02-23 19:53:25 +0900

    CreateMailContainerを作成

--- app/_components/create-mail/create-mail-container.tsx
+++ app/_components/create-mail/create-mail-container.tsx
@@ -1,16 +1,12 @@
 "use client";
-import { Tabs, Tab, Box } from "@mui/material";
+import { Tabs, Tab, Box, styled } from "@mui/material";
 import { useRouter, useSearchParams } from "next/navigation";
 import { User } from "@prisma/client";
 import { useEffect, useState } from "react";
 import TopTabPanel from "../common/tab/top-tab-panel";
 import BottomTabPanel from "../common/tab/bottom-tab-panel";
 
-export default function CreateMailContainer({
-  currentUser,
-}: {
-  currentUser: User | null;
-}) {
+const CreateMailContainer = ({ currentUser }: { currentUser: User | null }) => {
   const [tabValue, setTabValue] = useState("skip-class");
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -24,7 +20,7 @@ export default function CreateMailContainer({
     }
   }, [fffsearchParams]);
 
-  const handleChange = (event: any, newValue: any) => {
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     router.push(window.location.pathname + "/?category=" + newValue);
 
     // 2次リリースでshallow routing optionを実現したい
@@ -33,30 +29,59 @@ export default function CreateMailContainer({
   };
 
   return (
-    <Box>
-      {tabValue === "skip-class" && (
-        <TopTabPanel value={tabValue} currentUser={currentUser} />
-      )}
-      {tabValue === "cancel-job-offer" && (
-        <TopTabPanel value={tabValue} currentUser={currentUser} />
-      )}
-      {tabValue === "skip-part-time" && (
-        <TopTabPanel value={tabValue} currentUser={currentUser} />
-      )}
-      <Tabs value={tabValue} onChange={handleChange} centered>
-        <Tab label="大学の欠席" value="skip-class" />
-        <Tab label="内定取り消し" value="cancel-job-offer" />
-        <Tab label="アルバイト欠席" value="skip-part-time" />
-      </Tabs>
-      {tabValue === "skip-class" && (
-        <BottomTabPanel value={tabValue} currentUser={currentUser} />
-      )}
-      {tabValue === "cancel-job-offer" && (
-        <BottomTabPanel value={tabValue} currentUser={currentUser} />
-      )}
-      {tabValue === "skip-part-time" && (
-        <BottomTabPanel value={tabValue} currentUser={currentUser} />
-      )}
-    </Box>
+    <>
+      <TopTabPanel value={tabValue} currentUser={currentUser} />
+      <Box bgcolor={"#f7f7f7"}>
+        <StyledTabs value={tabValue} onChange={handleChange}>
+          <StyledTab label="大学の欠席" value="skip-class" />
+          <StyledTab label="内定取り消し" value="cancel-job-offer" />
+          <StyledTab label="アルバイト欠席" value="skip-part-time" />
+        </StyledTabs>
+      </Box>
+      <BottomTabPanel value={tabValue} currentUser={currentUser} />
+    </>
   );
+};
+
+interface StyledTabsProps {
+  value: string;
+  children?: React.ReactNode;
+  onChange: (event: React.SyntheticEvent, newValue: string) => void;
 }
+
+const StyledTabs = styled((props: StyledTabsProps) => (
+  <Tabs
+    {...props}
+    TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+    centered
+  />
+))({
+  "& .MuiTabs-indicator": {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "transparent",
+  },
+  "& .MuiTabs-indicatorSpan": {
+    maxWidth: 40,
+    width: "100%",
+    backgroundColor: "#016FE5",
+  },
+});
+
+type StyledTabProps = {
+  label: string;
+  value: string;
+};
+
+const StyledTab = styled((props: StyledTabProps) => (
+  <Tab disableRipple {...props} />
+))(({ theme }) => ({
+  fontWeight: theme.typography.fontWeightRegular,
+  fontSize: theme.typography.pxToRem(15),
+  color: "#707070",
+  "&.Mui-selected": {
+    color: "black",
+  },
+}));
+
+export default CreateMailContainer;
