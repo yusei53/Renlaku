b65e88e3b5bc916f3b8885362d4378c71063b382
yusei53 <yusei530922@gmail.com>
2024-02-25 19:59:32 +0900

    cardのリファクタリング

--- app/_components/lp/lp-third-view/solution-grid-card.tsx
+++ app/_components/lp/lp-third-view/solution-grid-card.tsx
@@ -6,27 +6,27 @@ const SolutionGridCard = () => {
   const isMediumScreen = useMediaQuery(theme.breakpoints.down("md"));
 
   return (
-    <Grid container spacing={1} padding={"2% 4%"}>
-      <Grid item xs={12} sm={12} md={12} lg={4} xl={4} py={2}>
+    <Grid container spacing={2} padding={"2% 4%"}>
+      <Grid item xs={12} lg={4} py={2}>
         <SolutionCard
           title="メールを高速で作成"
           firstContent="必要な項目のみ入力で"
-          secondContent="文が自動生成"
+          secondContent="文を簡単に作成"
         />
       </Grid>
-      <Grid item xs={12} sm={12} md={12} lg={4} xl={4} py={2}>
+      <Grid item xs={12} lg={4} py={2}>
         <SolutionCard
-          title="使い回し可能"
-          firstContent="プロフィール項目登録で"
-          secondContent="都度コピーするだけ"
+          title="誰でも簡単登録"
+          firstContent="プロフィール登録が"
+          secondContent="誰でも1分で完了"
           smallScreen={isMediumScreen ? false : true}
         />
       </Grid>
-      <Grid item xs={12} sm={12} md={12} lg={4} xl={4} py={2}>
+      <Grid item xs={12} lg={4} py={2}>
         <SolutionCard
-          title="バリエーションが豊富"
-          firstContent="学生がよく送るメールの"
-          secondContent="テンプレートを用意"
+          title="使い回し可能"
+          firstContent="プロフィール登録で"
+          secondContent="都度コピーするだけ"
           smallScreen={isMediumScreen ? false : true}
         />
       </Grid>
