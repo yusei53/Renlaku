1f1857675a25ddd0fb118ee84f934ee12b4ca329
yusei53 <yusei530922@gmail.com>
2024-02-28 18:15:27 +0900

    各ページにtitleタグを埋め込む

--- app/create-mail/page.tsx
+++ app/create-mail/page.tsx
@@ -1,8 +1,13 @@
+import { Metadata } from "next";
 import CreateMailContainer from "../_components/create-mail/create-mail-container";
 import getCurrentUser from "../_feature/actions/getCurrentUser";
 
-const Page = async () => {
+export const metadata: Metadata = {
+  title: "メール作成",
+};
+
+const CreateMailPage = async () => {
   const currentUser = await getCurrentUser();
   return <CreateMailContainer currentUser={currentUser} />;
 };
-export default Page;
+export default CreateMailPage;
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,12 @@
 import getCurrentUser from "../_feature/actions/getCurrentUser";
 import ProfileContainer from "../_components/profile/profile-container";
+import { Metadata } from "next";
 
-export const ProfilePage = async () => {
+export const metadata: Metadata = {
+  title: "プロフィール編集",
+};
+
+const ProfilePage = async () => {
   const currentUser = await getCurrentUser();
 
   return <ProfileContainer currentUser={currentUser} />;
