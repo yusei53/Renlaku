6318a73757123498b23763ca8017b42515a8f3ed
yusei53 <yusei530922@gmail.com>
2024-03-01 12:32:45 +0900

    ServiceCardを修正

--- app/_components/lp/lp-fourth-view/service-container.tsx
+++ app/_components/lp/lp-fourth-view/service-container.tsx
@@ -22,16 +22,18 @@ export const ServiceContainer = () => {
         content={
           "Renlakuは、メールを作成する時間を大幅に短縮するアプリです。煩雑な作業を解消し、あなたの大切な時間を節約します。また、一度ログインしてプロフィールを設定すれば、次回からは迅速にメールを作成できます。入力部分は特定箇所のみ。時間を大切に、コピー＆ペーストで手間を省きます。"
         }
-        image={"/man.png"}
+        image={"/student2.png"}
+        alt={"メール作成。たった30秒で完結"}
       />
       <ServiceCard
         number={"02"}
         firstTitle={"豊富な"}
         secondTitle={"テンプレート"}
         content={
-          "Renlakuは、大学生が直面するあらゆるシチュエーションに合わせたメールテンプレートを豊富に提供しています。現在大学やアルバイトの欠席連絡、内定取消しなど、大学生の日常に必要なテンプレートを提供しています。時代と共に変わる学生のニーズに柔軟に対応するために、常にユーザーからのリアルタイムのフィードバックを受け、テンプレートを継続的に追加・更新し続けます。"
+          "大学生が直面するあらゆるシチュエーションに合わせたメールテンプレートを豊富に提供しています。現在大学やアルバイトの欠席連絡、内定取消しなど、大学生の日常に必要なテンプレートを提供しています。時代と共に変わる学生のニーズに柔軟に対応するために、常にユーザーからのリアルタイムのフィードバックを受け、テンプレートを継続的に追加・更新し続けます。"
         }
-        image={"/man.png"}
+        image={"/mail.png"}
+        alt={"豊富なテンプレート"}
       />
       <ServiceCard
         number={"03"}
@@ -40,8 +42,9 @@ export const ServiceContainer = () => {
         content={
           "忙しい日々の中でもRenlakuでメール作成の時間を削減し、効率的な学生生活をサポート。大学生のニーズに特化し、文書作成の手間を減らしながら教授や人事に悪印象を与えないメールを作成します。ブックマーク機能でお気に入りのテンプレートをすぐに再利用。忙しいあなたの時間をもっと有効にしましょう。"
         }
-        image={"/man.png"}
+        image={"/student.png"}
         border={false}
+        alt={"使いやすさに特化。らくでストレスフリー"}
       />
     </Container>
   );
@@ -51,7 +54,7 @@ const Container = styled("div")(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
-  padding: "4% 20%",
+  padding: "4% 18%",
   paddingBottom: "5%",
   backgroundColor: "#f7f7f7",
   [theme.breakpoints.down("md")]: {
