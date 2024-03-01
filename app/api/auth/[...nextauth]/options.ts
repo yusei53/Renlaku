3e2f4539ce7410714f2bc15457145121ccbabeef
yusei53 <yusei530922@gmail.com>
2024-03-01 13:26:57 +0900

    NEXT_PUBLICを接頭辞につけた

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -57,7 +57,7 @@ const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_PUBLIC_SECRET,
 };
 
 export default authOptions;
