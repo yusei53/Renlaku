1475a6e1517076eee6fd6f0ed102f6168c1649d0
yusei53 <yusei530922@gmail.com>
2024-03-01 14:32:00 +0900

    元に戻す

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  NEXTAUTH_URL: "https://renlaku.vercel.app/",
-  NEXTAUTH_SECRET: "0BCDGUhicas+68+8MtNN9eIB0aS2k49xmusUg6b0K18=",
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
