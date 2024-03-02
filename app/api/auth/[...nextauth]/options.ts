0144d8acae6e6a3b993ff5a29b281d5a95a4e2a4
yusei53 <yusei530922@gmail.com>
2024-03-03 02:00:02 +0900

    不要なimport削除

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,4 +1,4 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/app/_lib/prisma";
 import GoogleProvider from "next-auth/providers/google";
