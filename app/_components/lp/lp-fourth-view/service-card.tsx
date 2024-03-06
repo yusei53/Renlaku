e79d166b5ba196d9d03e62c87e1319c1ffa6a9fd
yusei53 <yusei530922@gmail.com>
2024-03-06 19:06:34 +0900

    文字の大きさ修正

--- app/_components/lp/lp-fourth-view/service-card.tsx
+++ app/_components/lp/lp-fourth-view/service-card.tsx
@@ -69,7 +69,7 @@ const ServiceCard: React.FC<TProps> = ({
             </Typography>
           </Box>
         </Box>
-        <Typography fontSize={15} letterSpacing={0.5}>
+        <Typography fontSize={17} letterSpacing={0.5}>
           {content}
         </Typography>
       </Box>
