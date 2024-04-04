dbfcc7821bbb46387b82c844f4c685b70e2d0db2
yusei53 <yusei530922@gmail.com>
2024-04-04 23:05:03 +0900

    件名用のコンポーネント作成

---
+++ app/_components/skip-class/university-sub-mail-text.tsx
@@ -0,0 +1,22 @@
+import { forwardRef } from "react";
+import { Typography } from "@mui/material";
+import { SkipClassUserType } from "../../_feature/types/types";
+
+const UniversitySubMailText = forwardRef<HTMLDivElement, SkipClassUserType>(
+  ({ date, time, lesson }, ref) => {
+    return (
+      <Typography
+        ref={ref}
+        py={2}
+        px={{ xs: 3, sm: 5 }}
+        fontSize={{ xs: "15px", sm: "16px" }}
+      >
+        {date} {time}限{lesson}欠席のご連絡 <br />
+      </Typography>
+    );
+  }
+);
+
+UniversitySubMailText.displayName = "UniversitySubMailText";
+
+export default UniversitySubMailText;
