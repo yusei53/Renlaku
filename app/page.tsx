087d24bbc0031478d028b184fbfdcd4860892301
yusei53 <yusei530922@gmail.com>
2024-10-05 23:34:14 +0900

    add import React for fragment

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { QuestionContainer } from "./_components/lp/lp-second-view";
 import { SolutionContainer } from "./_components/lp/lp-third-view";
 import { ServiceContainer } from "./_components/lp/lp-fourth-view";
 import { SettingContainer } from "./_components/lp/lp-fifth-view";
+import React from "react";
 
 const Home = () => {
   return (
